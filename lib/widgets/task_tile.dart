8e3605cd88e45fe0c050a58d39822769c28e39ce
devnons <unimkeadie@gmail.com>
2022-04-05 06:38:37 +0100

    added checkbox functionality and text strikethrough.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,18 +1,56 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  void checkboxCallback(bool? checkboxState) {
+    setState(() {
+      isChecked = checkboxState!;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        onChanged: null,
-        value: false,
+    return ListTile(
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: checkboxCallback,
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({
+    Key? key,
+    required this.checkboxState,
+    required this.toggleCheckboxState,
+  }) : super(key: key);
+
+  final bool checkboxState;
+  final void Function(bool?) toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
